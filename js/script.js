7905c65034393cd81b70ba4d4b8141789eaa2dc4
atfdeenk <atfdeenk@gmail.com>
2024-11-13 17:40:24 +0700

    adding dark mode

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// sidenav
 function openNav() {
   document.getElementById('sidenav').style.width = '150px';
   // document.getElementById("main").style.marginRight = "250px";
@@ -8,3 +9,29 @@ function closeNav() {
   // document.getElementById("main").style.margin= "20px 40px";
   // document.getElementById("main").style.padding= "10px";
 }
+// sidenav end
+
+// dark mode
+// initial state
+ if (localStorage.getItem("darkMode") === "true") {
+  document.getElementById("checkbox-dark-mode").checked = true;
+  document.body.classList.add("dark-mode");
+}
+
+function toggleDarkMode() {
+  document.body.classList.toggle("dark-mode");
+  if (!document.getElementById("checkbox-dark-mode").checked) {
+    localStorage.setItem("darkMode", false);
+  } else {
+    localStorage.setItem("darkMode", true);
+  }
+}
+
+// let bulb = document.getElementById('bulb-dark');
+// function darkMode (params) {
+//   console.log(params);
+//   if (params == "dark") {
+//     bulb-darkMode.src = "/assets/dark mode icon/bulb-light.png"
+//   }
+// }
+// dark mode end
