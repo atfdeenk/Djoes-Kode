ca4479b53bce269541caf9192ae35ef11047e86f
atfdeenk <atfdeenk@gmail.com>
2024-11-13 16:00:15 +0700

    add funtioning hamburger menu

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function openNav() {
+  document.getElementById('sidenav').style.width = '150px';
+  // document.getElementById("main").style.marginRight = "250px";
+}
+
+function closeNav() {
+  document.getElementById('sidenav').style.width = '0';
+  // document.getElementById("main").style.margin= "20px 40px";
+  // document.getElementById("main").style.padding= "10px";
+}
