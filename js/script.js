9ba12e0121c51f32e91af1d6a1713a006d67329b
atfdeenk <atfdeenk@gmail.com>
2024-11-15 15:14:41 +0700

    add scroll to top

--- js/script.js
+++ js/script.js
@@ -59,3 +59,24 @@ function showSlides(n) {
   setTimeout(showSlides, 2000);
 }
 // carousel end
+
+// scroll to top
+let mybutton = document.getElementById('goTop');
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = 'block';
+  } else {
+    mybutton.style.display = 'none';
+  }
+}
+
+function goTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+// scroll to top end
